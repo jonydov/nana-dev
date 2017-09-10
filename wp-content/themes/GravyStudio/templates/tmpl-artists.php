6178cdf5197fd598640f6b5893aeb65a34acbcf9
leia <leiashh@gravystudio.com>
2017-09-10 18:22:51 +0300

    Artists Page - first version

--- wp-content/themes/GravyStudio/templates/tmpl-artists.php
+++ wp-content/themes/GravyStudio/templates/tmpl-artists.php
@@ -13,8 +13,46 @@ get_header();
 
 		<section class="section-artists">
 
-            <div class="shell">
-	            <?php
+			<div class="shell">
+
+                <div class="section-title">
+                    <?php if( get_sub_field('title') != null ){ ?>
+                        <h2><?php the_sub_field('title'); ?></h2>
+                    <?php } ?>
+                </div>
+
+                <div class="section-body">
+
+                    <div class="alphabet-scale">
+
+                        <?php
+                        $args = array(
+                            'posts_per_page'   => -1,
+                            'orderby'          => 'title',
+                            'order'            => 'ASC',
+                            'post_type'        => 'artists',
+                            'post_status'      => 'publish',
+                            'suppress_filters' => true
+
+                        );
+                        $posts_array = get_posts( $args );
+
+                        $first_letter= mb_substr($post->post_title,0,1);
+
+                        foreach ($posts_array as $post){
+
+                            $index_letter = mb_substr($post->post_title,0,1);
+
+                            if (strcasecmp($index_letter,$first_letter) !=0){
+                                $first_letter=$index_letter; ?>
+                                <div class="text"><span><?php echo $first_letter ?></span></div>
+
+                            <?php } ; ?>
+                        <?php } ?>
+
+                    </div>
+
+                    <?php
                     $args = array(
                         'posts_per_page'   => -1,
                         'orderby'          => 'title',
@@ -22,27 +60,48 @@ get_header();
                         'post_type'        => 'artists',
                         'post_status'      => 'publish',
                         'suppress_filters' => true
-                    );
 
+                    );
                     $posts_array = get_posts( $args );
 
+
+                    mb_internal_encoding("UTF-8");
+
+                    $first_letter= mb_substr($post->post_title,0,1);
+
                     foreach ($posts_array as $post){
+                        $img = get_the_post_thumbnail_url($post->ID);
+                    ?>
 
-	                    mb_internal_encoding("UTF-8");
-	                    $title = get_the_title();
-	                    $index_letter = mb_substr($string,0,1);
-                ?>
+                    <a href="<?=get_permalink($post->ID); ?>" class="item"
 
-                    <a class="item" name="<?php echo sanitize_title(get_the_title()); ?>" href="">
-                        <?php the_title(); ?><br/>
+                        <?php $index_letter = mb_substr($post->post_title,0,1);
+
+                        if (strcasecmp($index_letter,$first_letter) !=0){
+                            $first_letter=$index_letter; ?>
+                            name=index<?php echo $first_letter;  ?>
+                        <?php } ?> >
+
+                        <div class="image">
+                            <div class="holder" <?php if( $img != null ){ echo 'style="background-image: url('.$img.');"'; } ?>>
+                            </div>
+                        </div>
+
+
+                        <div class="text">
+                            <span class="name-artist"><?=$post->post_title; ?></span>
+                        </div>
                     </a>
 
-                <?php } ?>
-            </div>
+
+
+                    <?php } ?>
+                </div>
+			</div>
 
 		</section>
 
-	<?php } ?>
+	<?php }?>
 
 <?php endwhile; endif; ?>
 
