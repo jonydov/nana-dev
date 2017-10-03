6799b428b2112efeda773fc5b422fe6e235fe719
leia <leiashh@gravystudio.com>
2017-10-03 17:54:28 +0300

    artists & albums mobile

--- wp-content/themes/GravyStudio/functions.php
+++ wp-content/themes/GravyStudio/functions.php
@@ -19,6 +19,12 @@
         'description' => __( 'This sidebar is located above the age logo.'),
     ) );
 
+    register_sidebar( array(
+        'id'          => 'productions-archives',
+        'name'        => __( 'Productions Archives' ),
+        'description' => __( 'This sidebar is located above the age logo.'),
+    ) );
+
     function gs_enqueue_scripts() {
         wp_enqueue_script('jQuery', get_template_directory_uri()."/assets/js/jquery-2.2.4.min.js");
         wp_enqueue_script('slick', get_template_directory_uri()."/assets/js/slick.min.js");
@@ -215,6 +221,7 @@ function isRTL( $string ) {
 }
 
 add_action('wp_ajax_load-news', 'ajax_load_news');
+add_action('wp_ajax_load-productions', 'ajax_productions_news');
 
 function load_news($year, $month){
 
@@ -268,10 +275,70 @@ function load_news($year, $month){
 <?php $i++; }
 }
 
+function load_productions($year, $month){
+
+    $args = array(
+        'posts_per_page'   => -1,
+        'orderby'          => 'title',
+        'order'            => 'ASC',
+        'post_type'        => 'productions',
+        'post_status'      => 'publish',
+        'suppress_filters' => true,
+        'date_query' => array(
+            array(
+                'year'  => $year,
+                'month' => $month
+            ),
+        )
+    );
+
+    $items = get_posts( $args );
+    $i = 1;
+
+    foreach ( $items as $item ){ setup_postdata($item);
+
+        if( $i <= 4 ){
+            $class = 'style-1';
+        }else{
+            $class = 'style-2';
+        }
+        ?>
+
+        <a href="<?=get_permalink($item->ID); ?>" class="item animate fade-bottom <?=$class; ?>" data-delay="100">
+            <div class="holder">
+                <div class="image" style="background-image: url('<?=get_the_post_thumbnail_url($item->ID); ?>');">
+                    <div class="holder">
+                        <?php if( $class == 'style-1'){ ?>
+                            <span class="meta"><?=get_the_time('d בF Y' ,$item->ID); ?></span>
+                            <h3><?=get_the_title($item->ID); ?></h3>
+                        <?php } ?>
+                    </div>
+                </div>
+                <div class="text">
+                    <?php if( $class == 'style-2'){ ?>
+                        <span class="meta"><?=get_the_time('d בF Y' ,$item->ID); ?></span>
+                        <h3><?=get_the_title($item->ID); ?></h3>
+                    <?php } ?>
+                    <?php echo wp_trim_words($item->post_content, 18); ?>
+                </div>
+            </div>
+        </a>
+
+        <?php $i++; }
+}
+
 function ajax_load_news($year, $month){
+    $month = $_POST['month'];
+    $year = $_POST['year'];
+    load_news($year, $month);
+}
+
+
+
+function ajax_load_productions($year, $month){
 	$month = $_POST['month'];
 	$year = $_POST['year'];
-    load_news($year, $month);
+    load_productions($year, $month);
 }
 
 ?>
---
+++ wp-content/themes/GravyStudio/single-productions.php
@@ -0,0 +1,64 @@
+<?php
+get_header();
+$id = get_queried_object()->ID;
+?>
+
+<section class="section-productions-item">
+    <div class="shell">
+
+        <div class="section-top-bunner">
+
+	        <?php if ( has_post_thumbnail() ) { $img = get_the_post_thumbnail_url(); ?>
+                <div class="image" style="background-image: url('<?php echo $img; ?>');"></div>
+            <?php } ?>
+
+        </div>
+
+        <div class="section-body">
+
+            <div class="title"><h3><?= get_the_title(); ?></h3></div>
+            <span class="date"><?= get_the_time( 'd בM, Y' ); ?></span>
+            <div class="text">
+                <div class="holder">
+                    <div class="scrollbar" id="style-2">
+                        <div class="force-overflow">
+							<?php the_content(); ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="tag-list">
+				<?php
+				if ( get_the_tag_list() ) {
+					echo get_the_tag_list( '<ul><li>', '</li><li>', '</li></ul>' );
+				}
+				?>
+
+            </div>
+
+
+        </div>
+
+    </div>
+</section>
+
+
+<section class="section-music">
+
+    <div class="shell">
+
+        <div class="holder">
+
+            <div class="section-body">
+				<?php the_field( 'music' ); ?>
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+
+<?php get_footer(); ?>
