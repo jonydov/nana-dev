fc243d4464edc1308ae88d70af2a5532e3b2c644
jonydov <jonathan@gravystudio.com>
2017-11-20 11:11:31 +0200

    Schema markup for shows

--- wp-content/themes/GravyStudio/includes/item-shows.php
+++ wp-content/themes/GravyStudio/includes/item-shows.php
@@ -7,12 +7,13 @@ $date = $date->format( 'j.m' );
 <?php if ( strtotime( $today ) <= strtotime( get_field( 'date', $post->ID ) ) ) { ?>
     <div class="item" itemscope itemtype="http://schema.org/Event">
 
-        <span class="date">
+        <span class="date" itemprop="startDate" content="<?= get_field( 'date', $post->ID ); ?>">
             <?= $date; ?>
         </span>
-        <span class="time"  itemscope itemtype="http://schema.org/startDate><?= get_field( 'time', $post->ID ); ?> | </span>
 
-        <span class="artist"  itemscope itemtype="http://schema.org/performer">
+        <span class="time"><?= get_field( 'time', $post->ID ); ?> | </span>
+
+        <span class="artist" itemprop="performer">
             <?php
             $artists       = get_field( 'assigned_artist', $post->ID );
             $artists_names = array();
@@ -23,9 +24,10 @@ $date = $date->format( 'j.m' );
             ?>
         </span>
 
-        <span class="venue"  itemscope itemtype="http://schema.org/Place"><?= get_field( 'venue', $post->ID ); ?></span>
+        <span class="venue" itemprop="location" itemscope itemtype="http://schema.org/Place"><?= get_field( 'venue', $post->ID ); ?></span>
 
-        <span class="desc">, <?= get_the_title( $post->ID ); ?></span>
-        <span class="tix"><a target="_blank" href="<?= get_field( 'link', $post->ID ); ?>">כרטיסים</a></span>
+        <span class="desc" itemprop="name">, <?= get_the_title( $post->ID ); ?></span>
+
+        <span class="tix"><a itemprop="url" target="_blank" href="<?= get_field( 'link', $post->ID ); ?>">כרטיסים</a></span>
     </div>
 <?php } ?>
